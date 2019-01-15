0fa66db3e195a923a3cb4edd5918d3b9b571bd38
gwontaeyong <taaii6569@naver.com>
2019-01-15 11:12:23 +0900

    change Backjun project language to java

--- BackJun/Array/2920.py
+++
@@ -1,3 +0,0 @@
-input_ = list(map(int, input().split()))
-
-print(input_)
---
+++ BackJun/src/com/backjun/array/Main_2960.java
@@ -0,0 +1,10 @@
+package com.backjun.array;
+
+public class Main_2960 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

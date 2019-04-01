6990ff9f9fbc3c355afa124de1960396540cb573
taeyong <taaii6569@naver.com>
2019-04-02 07:55:41 +0900

    상시테스트

--- SamsungExpert/src/d4/n1949_등산로조정/Solution.java
+++ SamsungExpert/src/d4/n1949_등산로조정/Solution.java
@@ -9,11 +9,15 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-    static double map[][];
-    static int n;
-    static int k;
+    static int map[][];
+    static boolean visit[][];
 
-    static int dx [] = {1, -1, 0, 0};
+    static int n;
+    static int K;
+    static int answer;
+    static int ans;
+
+    static int dx[] = {1, -1, 0, 0};
     static int dy[] = {0, 0, -1, 1};
 
     static class Rode {
@@ -41,42 +45,102 @@ public class Solution {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            map = new int[n][n];
+            answer = 0;
+            ans = 0;
+            int max = 0;
 
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
 
                 for (int j = 0; j < n; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
+                    max = Math.max(max, map[i][j]);
                 }
             }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (map[i][j] == max) {
+                        boolean visited[][] = new boolean[n][n];
+                        //visit = new boolean[n][n];
+                        //dfs(i, j, 1, false);
+                        dfs(visited, j, i, 1, false);
+                    }
+                }
+            }
+
+            System.out.println(String.format("#%d %d", tc, answer));
 
         }
     }
 
-    public static void dfs(int map[][], int x, int y, int count, boolean isCut) {
+    private static void dfs(int y, int x, int len, boolean cut) {
+        //cut이 true 면 공사를 했다.
 
+        visit[y][x] = true;
+//      if (map[y][x] == bottom) {
+        answer = answer < len ? len : answer;
+//          return;
+//      }
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (!isRange(nx, ny)|| visit[ny][nx])
+                continue;
+            if (map[ny][nx] < map[y][x]) {
+                dfs(ny, nx, len + 1, cut);
+            } else { //같거나 높으면
+                if (!cut) { //cut이 false면 아직 공사를 안한 상태
+                    for (int k = 1; k <= K; k++) {
+                        int tmp = map[ny][nx];
+                        map[ny][nx] -= k;
+                        if (map[ny][nx] < map[y][x])
+                            dfs(ny, nx, len + 1, true);
+                        map[ny][nx] = tmp;
+                    }
+                }
+            }
+            visit[ny][nx] = false;
+        }
+
+    }
+
+    public static void dfs(boolean visited[][], int x, int y, int count, boolean isCut) {
+
+        visited[y][x] = true;
+        answer = Math.max(answer, count);
 
         for (int i = 0; i < 4; i++) {
-
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if(map[ny][nx] <= map[y][x]){// 다음에 갈 등산로가 현재보다 작을 경우
 
-                if(isCut){
-                    // 최대값 확인
-                    continue;
-                }else{
-                    //현재 등산로의 높이를 k가지로 변경
+            if (!isRange(nx, ny) || visited[ny][nx])
+                continue;
+
+            if (map[ny][nx] < map[y][x]) {// 다음에 갈 등산로가 현재보다 작을 경우
+                dfs(visited, nx, ny, count + 1, isCut);
+            } else {
+                if (!isCut) {
+                    for (int k = 1; k <= K; k++) {
+                        int tmp = map[ny][nx];
+                        map[ny][nx] -= k;
+                        if (map[ny][nx] < map[y][x])
+                            dfs(visited, nx, ny, count + 1, true);
+                        map[ny][nx] = tmp;
+                    }
                 }
             }
-
-
-
+            visited[ny][nx] = false;
         }
+    }
 
-
+    public static boolean isRange(int x, int y) {
+        if (x < 0 || y < 0 || x >= n || y >= n)
+            return false;
+        return true;
     }
 
 }
---
+++ SamsungExpert/src/d4/n5658_보물상자/Solution.java
@@ -0,0 +1,68 @@
+package d4.n5658_보물상자;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution {
+
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(Solution.class.getResourceAsStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+
+        for (int t = 1; t <= T; t++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            char arr[] = br.readLine().toCharArray();
+
+            Set<String> set = new HashSet<>();
+
+
+            int len = N / 4;
+
+            for (int i = 0; i < len; i++) {
+                
+                int count = 0;
+                String str = "";
+
+                for (int idx = i; idx < i + N; idx++) {
+                    count++;
+                    str += arr[idx % N];
+
+                    if (count == len) {
+                        count = 0;
+                        set.add(str);
+                        str = "";
+                    }
+                }
+
+            }
+
+            ArrayList<Long> list = new ArrayList<>();
+            List<String> strList = new ArrayList<>();
+
+            strList.addAll(set);
+
+            for (int i = 0; i < strList.size(); i++) {
+                String str = strList.get(i);
+                list.add(Long.parseLong(str, 16));
+            }
+
+            Collections.sort(list, Collections.reverseOrder());
+
+            System.out.println(String.format("#%d %d", t, list.get(K - 1)));
+        }
+    }
+
+
+}
